137eb2dcb93416c799b276521700f121c83f7a8c
Unknown <arthur.delamare1@viacesi.fr>
2020-08-19 12:37:39 +0200

    feat(terminal): implement terminal spinners

--- src/cli.js
+++ src/cli.js
@@ -1,18 +1,21 @@
 require("dotenv").config();
 const program = require("commander");
 const TickTickAPI = require("ticktick-node-api");
+const ora = require("ora");
 
 async function execute() {
   const api = new TickTickAPI();
 
-  console.log("Connexion à TickTick.");
+  const connectSpinner = ora("Connexion à TickTick").start();
   await api.login({
     username: process.env.TICKTICK_USERNAME,
     password: process.env.TICKTICK_PASSWORD,
   });
+  connectSpinner.succeed();
 
-  console.log("Récupération des tâches.");
+  const goalsSpinner = ora("Récupération des objectifs").start();
   const tasks = await api.getTasks({ name: "Ligue", status: 0 });
+  goalsSpinner.succeed();
 
   console.log();
   console.log("Objectifs du jour :");
