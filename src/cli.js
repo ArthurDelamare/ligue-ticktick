afff6191a6d1ac4fdb79b38cffadefcc2e0797cd
Unknown <arthur.delamare1@viacesi.fr>
2020-08-19 11:44:35 +0200

    feat(command): renaming + waiting sentences

--- src/cli.js
+++ src/cli.js
@@ -5,19 +5,22 @@ const TickTickAPI = require("ticktick-node-api");
 async function execute() {
   const api = new TickTickAPI();
 
+  console.log("Connexion à TickTick.");
   await api.login({
     username: process.env.TICKTICK_USERNAME,
     password: process.env.TICKTICK_PASSWORD,
   });
 
+  console.log("Récupération des tâches.");
   const tasks = await api.getTasks({ name: "Ligue", status: 0 });
 
+  console.log();
   console.log("Objectifs du jour :");
   for (const task of tasks) {
     console.log(`:construction: ${task.title}`);
   }
 }
 
-program.command("test").action(() => execute());
+program.command("get-goals").action(() => execute());
 
 program.parse(process.argv);
